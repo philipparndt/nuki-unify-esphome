9c2d3c98f84ea1f4097ac176ea826267e24ec551
Philipp Arndt <philipp.arndt@vector.com>
2025-02-13 08:52:23 +0100

    initial

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sops -d secrets.enc.yaml > secrets.yaml
 docker run --rm -v "${PWD}":/config -it esphome/esphome compile app.yaml
-cp .esphome/build/1wire_sml/.pioenvs/1wire_sml/firmware.bin ./1wire_sml_firmware.bin
+cp .esphome/build/unifi_nuki/.pioenvs/unifi_nuki/firmware.bin ./unifi_nuki_firmware.bin
